e929e76874de7f74607fb6b940cf1f8bb2e98302
Plamenpowa <pkostadinov72@gmail.com>
2022-12-13 00:07:03 +0200

    i broke my neck couple of time

--- src/js/Application.js
+++ src/js/Application.js
@@ -9,12 +9,34 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
-
+    this._loading = document.querySelector("#progress");
     this.emit(Application.events.READY);
+    this._load();
+    this._loading.value = 0;
   }
+  async _load() {
+    const res = await fetch("https://swapi.boom.dev/api/planets");
+    const data = await res.json();
+    this._loading.max = data.count;
+    for (let i = 0; i < 6; i++) {
+      this._startLoading(i);
+    }
+  }
+  _create(planetName) {
+    const box = document.createElement("div");
+    box.textContent = planetName;
+    document.querySelector("#list").appendChild(box);
+  }
+  async _startLoading(page) {
+    const res = await fetch(`https://swapi.boom.dev/api/planets?page=${page}`);
+    const data = await res.json();
+    data.results.forEach((planet) => {
+      this._create(planet.name);
+      this._loading.value++;
+    });
+  }
+  _stopLoading() {
+    this._loading.style.display = "none";
+  }
+  _render() {}
 }
